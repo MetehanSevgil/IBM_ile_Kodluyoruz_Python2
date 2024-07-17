f29753b07d72e96c001364b7034fa19f3d935b4a
MetehanSevgil <147412107+MetehanSevgil@users.noreply.github.com>
2024-07-17 14:22:42 +0300

    Update Oklid.py

--- Oklid.py
+++ Oklid.py
@@ -1,7 +1,7 @@
 import math 
 
-def ouclide(x1: float, y1: float, x2:float, y2:float):
-    # p = sqrt(q^2 + r^2)
+def ouclide(x1: float, y1: float, x2: float, y2: float):
+    #p = sqrt(q^2 + r^2)
     return math.sqrt((x2 -x1)**2 + (y2-y1)**2)
 
 if __name__ == "__main__":
